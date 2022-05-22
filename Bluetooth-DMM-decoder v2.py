ea0ad34020f88df06b830b911cb6d56b8dcf86a1
ShiroNeko <30852897+Shiro-Nek0@users.noreply.github.com>
2022-05-22 03:06:37 -0400

    Add files via upload

--- Bluetooth-DMM-decoder v2.py
+++ Bluetooth-DMM-decoder v2.py
@@ -1,6 +1,5 @@
-from gettext import translation
-from unicodedata import digit
-
+import SAMPLEDATA
+import random
 
 test_encoded_handle = "0x0009" #?
 
@@ -15,6 +14,18 @@ digits = {
 	"1010010":7,
 	"1111111":8,
 	"1111011":9,
+	
+	"0110010":"asdsadsad",
+	"0111111":"asdsadsad",
+	"1011000":"asdsadsad",
+
+	"0100101":"L",
+	"1010101":" ",
+	"1111110":"A",
+	"0000111":"u",
+	"0101101":"t",
+	"0001111":"o",
+	"0000000":" "
 }
 
 bits2 = [
@@ -26,18 +37,25 @@ bits2 = [
 xorkey = [0x41,0x21,0x73,0x55,0xa2,0xc1,0x32,0x71,0x66,0xaa,0x3b,0xd0,0xe2,0xa8,0x33,0x14,0x20,0x21,0xaa,0xbb]
 
 testvalue = [0x1b, 0x84, 0x70, 0xb1, 0x49, 0x6a, 0x9f, 0x3c, 0x66, 0xaa, 0x3b] #TEST
-
 def xor(x,y):
 	return x ^ y
 
 def hex_to_binary(x):
 	return bin(int(x, base=16)).lstrip('0b').zfill(8)
 
-def flip_binary_order(x):
+def LSB_TO_MSB(x):
 	return x[::-1]
 
-def rearange_bits(bits):
-	reorder = [0,3,2,7,6,1,5,4] #7 segment display order
+# AAA
+# B C
+# DDD
+# E F
+# GGG
+# (Minus D1e D1b D1a D1g D1f D1d D1c)
+# ADCHGBFE = 03276154
+
+def rearange_bits(bits):#REORDER TO STANDARD SEVSEG PROTOCOL
+	reorder = [0,3,2,7,6,1,5,4] #7 segment display order SevSeg protocol
 	rearranged = []
 	digit = ""
 	for x in range(len(bits)):
@@ -48,48 +66,39 @@ def rearange_bits(bits):
 				digit = ""
 	return rearranged
 
-fullbinary = ""
-binary_array = []
+def decode_all(hex_data):
+	fullbinary = ""
+	binary_array = []
+	for x in range(len(hex_data)):
+		#tohex = hex(hex_data[x]) #in case data its already XOR decoded
+		tohex = hex(xor(hex_data[x],xorkey[x])) #Bytewyse XOR operation
+		tobinary = hex_to_binary(tohex) #convert to binary
+		flipped = LSB_TO_MSB(tobinary) #reverse binary
+		binary_array.append(flipped)
+		fullbinary += flipped
+	digit_segment = fullbinary[28:60] #7 segment display
+	symbols_section = fullbinary[60:88] #symbols
+	segment_array = []
+	for x in range(0,len(digit_segment),8): #split into 8 bit segments
+		segment_array.append(digit_segment[x:x+8])
+	#rearange bits
+	rearranged_segment_array = rearange_bits(segment_array) #rearrange bits
+	print(rearranged_segment_array)
+	display = ""
+	if fullbinary[28] == "1":
+		display += "-"
+	for x in rearranged_segment_array:
+		if x[0] == "1":
+			display += "."
+		display += str(digits[x[1:8]])
+	for x in range(len(symbols_section)):
+		if symbols_section[x] == "1":
+			display += str(bits2[x])+" "
+	return display
 
-for x in range(len(testvalue)):
-	tohex = hex(xor(testvalue[x],xorkey[x])) #Bytewyse XOR operation
-	tobinary = hex_to_binary(tohex) #convert to binary
-	flipped = flip_binary_order(tobinary) #reverse binary
-	binary_array.append(flipped)
-	fullbinary += flipped
-	print(f"tohex: {tohex}")
-	print(f"tobinary: {tobinary}")
-	print(f"flipped: {flipped}")
-	print(f"fullbinary: {fullbinary}")
 
-digit_segment = fullbinary[28:60] #7 segment display
-symbols_section = fullbinary[60:88] #symbols
+print(f"display: {decode_all(testvalue)}")
 
-print(f"digit_segment: {digit_segment}")
+#for x in SAMPLEDATA.data: print(f"display: {decode_all(x)}")#alredy xor decoded data
 
-
-segment_array = []
-
-for x in range(0,len(digit_segment),8): #split into 8 bit segments
-	segment_array.append(digit_segment[x:x+8])
-	
-print(f"segment_array: {segment_array}")
-
-#rearange bits
-rearranged_segment_array = rearange_bits(segment_array) #rearrange bits
-print(f"rearranged_segment_array: {rearranged_segment_array}")
-
-
-display = ""
-
-for x in rearranged_segment_array:
-	display += str(digits[x[1:8]])
-	if x[0] == "1":
-		display += "."
-	
-for x in range(len(symbols_section)):
-	if symbols_section[x] == "1":
-		display += str(bits2[x])
-	
-
-print(f"display: {display}")
+#print(digits[rearange_bits(["01111101"])[0][1:8]]) #from binary to sevgev to decimal
